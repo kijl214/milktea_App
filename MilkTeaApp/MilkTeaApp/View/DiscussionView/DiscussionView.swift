e767014ce483683d223aa84cf298ab034ae1a628
kijl214 <WongKinMan22@yahoo.com.hk>
2023-01-12 13:52:42 +0800

    Add DiscussionView.

---
+++ MilkTeaApp/MilkTeaApp/View/DiscussionView/DiscussionView.swift
@@ -0,0 +1,20 @@
+//
+//  DiscussionView.swift
+//  MilkTeaApp
+//
+//  Created by ituser on 12/1/2023.
+//
+
+import SwiftUI
+
+struct DiscussionView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct DiscussionView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiscussionView()
+    }
+}
