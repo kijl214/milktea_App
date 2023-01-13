655c403a02565b1be578b064ebddfb321117a607
kijl214 <WongKinMan22@yahoo.com.hk>
2023-01-13 11:11:15 +0800

    add Text and image in the MapView.

--- MilkTeaApp/MilkTeaApp/View/MapView/MapView.swift
+++ MilkTeaApp/MilkTeaApp/View/MapView/MapView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct MapView: View {
     var body: some View {
-        NavigationView{Text("Hello, World!")}
+        NavigationView{
+            VStack{
+            Image("milktea")
+            Text("The feature of Hong Kong-style Milk Tea is the filter bags for tea. The tea bag's colour and shape are very similar to stockings after prolonged use, so Hong Kong-style milk tea is also known as stocking milk tea.")
+            }
+        }
         
     }
 }
